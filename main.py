7c6c04a4cbbcdb3373fe8cb34a850ed08cf37426
ftem2002 <91579449+ftem2002@users.noreply.github.com>
2021-12-23 23:00:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+num1 = float(input("please enter the first num :"))
+num2 =float(input("please enter the second num :"))
+operator = input ("please enter the operator :")
+if operator == "+":
+    print(num1+num2)
+elif operator == "-":
+    print(num1-num2)
+elif operator=="*":
+    print(num1*num2)
+elif operator == "/":
+    print(num1/num2)
+else: print("wrong operator")
+
